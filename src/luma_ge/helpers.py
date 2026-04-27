4bc1bde2ce18b8eb166b24b48be86ebecc0c3ced
Agil Akbar Fahrezi <agilakbarfahrezi@mail.ugm.ac.id>
2026-04-27 11:33:43 +0700

    Delete AOI for gaul function

--- src/luma_ge/helpers.py
+++ src/luma_ge/helpers.py
@@ -488,27 +488,3 @@ class EE_Export_Manager:
         removed           = len(self.export_tasks) - len(keep)
         self.export_tasks = keep
         return removed
-
-
-#############################  Area of Interest  ###########################
-def get_aoi_from_gaul(country="Indonesia", province="Sumatera Selatan"):
-    """
-    Get Area of Interest geometry from GAUL administrative boundaries.
-    
-    Parameters:
-    -----------
-    country : str
-        Country name (default: "Indonesia")
-    province : str
-        Province/state name (default: "Sumatera Selatan")
-        
-    Returns:
-    --------
-    ee.Geometry : Area of interest geometry
-    """
-    ensure_ee_initialized()
-    admin = ee.FeatureCollection("FAO/GAUL/2015/level1")
-    aoi_fc = admin.filter(ee.Filter.eq('ADM0_NAME', country)).filter(
-        ee.Filter.eq('ADM1_NAME', province)
-    )
-    return aoi_fc.geometry()
