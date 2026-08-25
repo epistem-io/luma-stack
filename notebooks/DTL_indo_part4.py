938eee603e07d72655a5dc40a38b79dbc5bdb6d1
Maria Adelia <mariaadeliawidijanto@gmail.com>
2026-08-25 20:52:08 +0200

    sulawesi v6 pipeline

--- notebooks/DTL_indo_part4.py
+++ notebooks/DTL_indo_part4.py
@@ -39,12 +39,12 @@ def asset_exists(asset_id):
 
 def main():
 
-    provinces = ee.FeatureCollection('projects/epistem2/assets/AOI_JawaBali_Provinces')
+    provinces = ee.FeatureCollection('projects/epistem2/assets/AOI_Sulawesi_Provinces')
     province_list = provinces.toList(provinces.size())
-    n_provinces = 7 # jawabali number of provinces
+    n_provinces = 6 # sulawesi number of provinces
 
     # for i in range(n_provinces):
-    for i in range(0, n_provinces):
+    for i in range(4, n_provinces):
         province = ee.Feature(province_list.get(i))
         province_name = province.get('AoI').getInfo()
         province_name_clean = (
